84dc9e82aa05eb345839daaced869ddc3bb6244d
JeffersonGalv <jefdhenes@gmail.com>
2024-06-27 12:25:12 -0300

    Agora templates!

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def portao(request):
+    return HttpResponse("Você chegou ao partão da casa.")
+
+
+def sala(request):
+    return HttpResponse("Você chegou na sala. Sente-se no sofá!")
+
+
+def quarto(request):
+    return HttpResponse("Agora esta no quarto, pode deitar.")
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
